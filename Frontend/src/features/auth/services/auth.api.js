f67d5a1bf78b025facc60a21b4244baf68bfea08
Aditya Singh <adityasinghbarswal7@gmail.com>
2026-06-17 12:12:37 +0530

    feat: auth api service layer with axios instance

---
+++ Frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+// Axios Instance
+const api = axios.create({
+  baseURL: "http://localhost:3000/api/auth",
+  withCredentials: true,
+});
+
+// Register API
+export async function registerUser({ username, email, password }) {
+  try {
+    const response = await api.post("/register", {
+      username,
+      email,
+      password,
+    });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Login API
+export async function loginUser({ email, password }) {
+  try {
+    const response = await api.post("/login", {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Logout API
+export async function logoutUser() {
+  try {
+    const response = await api.post("/logout", {});
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Get Me API
+export async function getMe() {
+  try {
+    const response = await api.get("/get-me");
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
