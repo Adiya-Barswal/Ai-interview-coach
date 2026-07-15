497ca291ac988789275f85560a5831ec79553892
Aditya Singh <adityasinghbarswal7@gmail.com>
2026-07-15 22:01:18 +0530

    Fix production auth and server port

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,8 @@ const app = require("./app");
 const connectToDb = require("./src/config/database");
 
 connectToDb();
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
